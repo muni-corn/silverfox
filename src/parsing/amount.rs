2e6e5c549507bd64813f75033b477ff082863859
municorn <municorn@musicaloft.com>
2021-04-22 15:33:26 -0600

    parsing: Improvements to amount parsers

--- src/parsing/amount.rs
+++ src/parsing/amount.rs
@@ -1,4 +1,5 @@
 use nom::bytes::complete::take_while1;
+use nom::character::complete::space0;
 use nom::character::complete::space1;
 use nom::combinator::map;
 use nom::sequence::pair;
@@ -15,10 +16,8 @@ use super::is_amount_symbol_char;
 // pub fn parse_amount(input: &str) -> SilverfoxResult<Amount> {
 //     let (leftover, (symbol_raw, quantity_raw)) = {
 //         alt((
-//             symbol_then_number_together,
-//             map(number_then_symbol_separate, |(x, y)| (y, x)),
-//             symbol_then_number_separate,
-//             map(number_then_symbol_together, |(x, y)| (y, x)),
+//             symbol_then_number,
+//             map(number_then_symbol, |(x, y)| (y, x)),
 //             number_only,
 //         ))
 //     }(input).map_err(|_| ParseError {
@@ -40,20 +39,12 @@ use super::is_amount_symbol_char;
 //     })
 // }
 
-fn symbol_then_number_separate(input: &str) -> IResult<&str, (&str, &str)> {
-    separated_pair(symbol_only, space1, number_only)(input)
+fn symbol_then_number(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(symbol_only, space0, number_only)(input)
 }
 
-fn number_then_symbol_separate(input: &str) -> IResult<&str, (&str, &str)> {
-    separated_pair(number_only, space1, symbol_only)(input)
-}
-
-fn symbol_then_number_together(input: &str) -> IResult<&str, (&str, &str)> {
-    pair(symbol_only, number_only)(input)
-}
-
-fn number_then_symbol_together(input: &str) -> IResult<&str, (&str, &str)> {
-    pair(number_only, symbol_only)(input)
+fn number_then_symbol(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(number_only, space0, symbol_only)(input)
 }
 
 fn number_only(input: &str) -> IResult<&str, &str> {
@@ -69,54 +60,54 @@ mod tests {
 
     #[test]
     fn test_symbol_then_number_separate() {
-        assert_eq!(symbol_then_number_separate("$ 123"), Ok(("", ("$", "123"))));
-        assert_eq!(symbol_then_number_separate("Rs 123"), Ok(("", ("Rs", "123"))));
-        assert_eq!(symbol_then_number_separate("BTC 123"), Ok(("", ("BTC", "123"))));
-        assert_eq!(symbol_then_number_separate("p 123,92"), Ok(("", ("p", "123,92"))));
-        assert_eq!(symbol_then_number_separate("h 1 "), Ok((" ", ("h", "1"))));
-        assert_eq!(symbol_then_number_separate("$ 100 extra stuff"), Ok((" extra stuff", ("$", "100"))));
-        assert!(symbol_then_number_separate(" h 1").is_err());
-        assert!(symbol_then_number_separate("12").is_err());
-        assert!(symbol_then_number_separate("$").is_err());
+        assert_eq!(symbol_then_number("$ 123"), Ok(("", ("$", "123"))));
+        assert_eq!(symbol_then_number("Rs 123"), Ok(("", ("Rs", "123"))));
+        assert_eq!(symbol_then_number("BTC 123"), Ok(("", ("BTC", "123"))));
+        assert_eq!(symbol_then_number("p 123,92"), Ok(("", ("p", "123,92"))));
+        assert_eq!(symbol_then_number("h 1 "), Ok((" ", ("h", "1"))));
+        assert_eq!(symbol_then_number("$ 100 extra stuff"), Ok((" extra stuff", ("$", "100"))));
+        assert!(symbol_then_number(" h 1").is_err());
+        assert!(symbol_then_number("12").is_err());
+        assert!(symbol_then_number("$").is_err());
     }
 
     #[test]
     fn test_number_then_symbol_separate() {
-        assert_eq!(number_then_symbol_separate("123 $"), Ok(("", ("123", "$"))));
-        assert_eq!(number_then_symbol_separate("123 Rs"), Ok(("", ("123", "Rs"))));
-        assert_eq!(number_then_symbol_separate("123 BTC"), Ok(("", ("123", "BTC"))));
-        assert_eq!(number_then_symbol_separate("123,92 p"), Ok(("", ("123,92", "p"))));
-        assert_eq!(number_then_symbol_separate("1 h "), Ok((" ", ("1", "h"))));
-        assert_eq!(number_then_symbol_separate("100 $ extra stuff"), Ok((" extra stuff", ("100", "$"))));
-        assert!(number_then_symbol_separate(" 1 h").is_err());
-        assert!(number_then_symbol_separate("12").is_err());
-        assert!(number_then_symbol_separate("$").is_err());
+        assert_eq!(number_then_symbol("123 $"), Ok(("", ("123", "$"))));
+        assert_eq!(number_then_symbol("123 Rs"), Ok(("", ("123", "Rs"))));
+        assert_eq!(number_then_symbol("123 BTC"), Ok(("", ("123", "BTC"))));
+        assert_eq!(number_then_symbol("123,92 p"), Ok(("", ("123,92", "p"))));
+        assert_eq!(number_then_symbol("1 h "), Ok((" ", ("1", "h"))));
+        assert_eq!(number_then_symbol("100 $ extra stuff"), Ok((" extra stuff", ("100", "$"))));
+        assert!(number_then_symbol(" 1 h").is_err());
+        assert!(number_then_symbol("12").is_err());
+        assert!(number_then_symbol("$").is_err());
     }
 
     #[test]
     fn test_symbol_then_number_together() {
-        assert_eq!(symbol_then_number_together("$123"), Ok(("", ("$", "123"))));
-        assert_eq!(symbol_then_number_together("Rs123"), Ok(("", ("Rs", "123"))));
-        assert_eq!(symbol_then_number_together("BTC123"), Ok(("", ("BTC", "123"))));
-        assert_eq!(symbol_then_number_together("p123,92"), Ok(("", ("p", "123,92"))));
-        assert_eq!(symbol_then_number_together("h1 "), Ok((" ", ("h", "1"))));
-        assert_eq!(symbol_then_number_together("$100 extra stuff"), Ok((" extra stuff", ("$", "100"))));
-        assert!(symbol_then_number_together(" h1").is_err());
-        assert!(symbol_then_number_together("12").is_err());
-        assert!(symbol_then_number_together("$").is_err());
+        assert_eq!(symbol_then_number("$123"), Ok(("", ("$", "123"))));
+        assert_eq!(symbol_then_number("Rs123"), Ok(("", ("Rs", "123"))));
+        assert_eq!(symbol_then_number("BTC123"), Ok(("", ("BTC", "123"))));
+        assert_eq!(symbol_then_number("p123,92"), Ok(("", ("p", "123,92"))));
+        assert_eq!(symbol_then_number("h1 "), Ok((" ", ("h", "1"))));
+        assert_eq!(symbol_then_number("$100 extra stuff"), Ok((" extra stuff", ("$", "100"))));
+        assert!(symbol_then_number(" h1").is_err());
+        assert!(symbol_then_number("12").is_err());
+        assert!(symbol_then_number("$").is_err());
     }
 
     #[test]
     fn test_number_then_symbol_together() {
-        assert_eq!(number_then_symbol_together("123$"), Ok(("", ("123", "$"))));
-        assert_eq!(number_then_symbol_together("123Rs"), Ok(("", ("123", "Rs"))));
-        assert_eq!(number_then_symbol_together("123BTC"), Ok(("", ("123", "BTC"))));
-        assert_eq!(number_then_symbol_together("123,92p"), Ok(("", ("123,92", "p"))));
-        assert_eq!(number_then_symbol_together("1h "), Ok((" ", ("1", "h"))));
-        assert_eq!(number_then_symbol_together("100$ extra stuff"), Ok((" extra stuff", ("100", "$"))));
-        assert!(number_then_symbol_together(" 1h").is_err());
-        assert!(number_then_symbol_together("12").is_err());
-        assert!(number_then_symbol_together("$").is_err());
+        assert_eq!(number_then_symbol("123$"), Ok(("", ("123", "$"))));
+        assert_eq!(number_then_symbol("123Rs"), Ok(("", ("123", "Rs"))));
+        assert_eq!(number_then_symbol("123BTC"), Ok(("", ("123", "BTC"))));
+        assert_eq!(number_then_symbol("123,92p"), Ok(("", ("123,92", "p"))));
+        assert_eq!(number_then_symbol("1h "), Ok((" ", ("1", "h"))));
+        assert_eq!(number_then_symbol("100$ extra stuff"), Ok((" extra stuff", ("100", "$"))));
+        assert!(number_then_symbol(" 1h").is_err());
+        assert!(number_then_symbol("12").is_err());
+        assert!(number_then_symbol("$").is_err());
     }
 
     #[test]
@@ -129,5 +120,15 @@ mod tests {
         assert_eq!(number_only("123 BTC"), Ok((" BTC", "123")));
         assert!(number_only(" 123").is_err());
         assert!(number_only("$123").is_err());
+    }
+
+    #[test]
+    fn test_symbol_only() {
+        assert_eq!(symbol_only("$"), Ok(("", "$")));
+        assert_eq!(symbol_only("$100"), Ok(("100", "$")));
+        assert_eq!(symbol_only("$.10"), Ok((".10", "$")));
+        assert!(symbol_only(" $").is_err());
+        assert!(symbol_only("100$").is_err());
+        assert!(symbol_only(" 100$").is_err());
     }
 }
